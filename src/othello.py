49f5798728068ba764eef3bb89990eae90c0ea36
ryuichi1208 <ryucrosskey@gmail.com>
2018-10-10 22:40:08 +0900

    thinking fix

--- src/othello.py
+++ src/othello.py
@@ -6,6 +6,7 @@ import random
 import copy
 
 
+#
 class Othello:
 
     def play(self):
@@ -13,8 +14,6 @@ class Othello:
         board = Board()
         player1 = computer = Computer(BLACK, "PC")
         player2 = user = User(WHITE, "あなた")
-        #player1 = user = User(BLACK, "あなた")
-        #player2 = computer = Computer(WHITE, "PC")
 
         turn = 0
         hand1 = hand2 = None
@@ -24,11 +23,11 @@ class Othello:
 
             print(board)
             hand1 = player1.play(board)
-            print("%sの手: %s" % (player1.name, hand1))
+            print("%s Play: %s" % (player1.name, hand1))
 
             print(board)
             hand2 = player2.play(board)
-            print("%sの手: %s" % (player2.name, hand2))
+            print("%s Play: %s" % (player2.name, hand2))
 
         self.show_result(board)
 
